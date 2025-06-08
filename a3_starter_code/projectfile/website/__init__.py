07bdd4f16aa3246ed9eae3cf52b2bbc68ffae061
David Clark <david.clark172@hotmail.com>
2025-06-08 18:23:45 +1000

    Error Handling

--- a3_starter_code/projectfile/website/__init__.py
+++ a3_starter_code/projectfile/website/__init__.py
@@ -42,4 +42,15 @@ def create_app():
     from . import auth
     app.register_blueprint(auth.auth_bp)
     
+    @app.errorhandler(404)
+    def not_found(error):
+        return render_template('404.html'), 404
+    
+    @app.errorhandler(500)
+    def internal_error(error):
+        return render_template('500.html'), 500
+    
+    with app.app_context():
+        db.create_all()
+    
     return app
